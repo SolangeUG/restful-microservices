d0fd2125be1a27a5759040d81ed692d5834d4425
Solange Umuhire GASENGAYIRE <sgasengayire@gmail.com>
2018-01-20 10:43:23 +0100

    Add application minimal functionality

--- src/main/java/org/java/training/App.java
+++ src/main/java/org/java/training/App.java
@@ -1,13 +1,38 @@
 package org.java.training;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Optional;
+import java.util.Properties;
+
 /**
- * Hello world!
+ * Spring Boot application to manage a company's employees
+ * via Java based RESTful microservices.
  *
+ * @author Solange U. Gasengayire
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+@SpringBootApplication
+public class App {
+
+    // Get HOST and PORT from environment or set default
+    private static final Optional<String> host;
+    private static final Optional<String> port;
+    private static final Properties appProps = new Properties();
+
+    static {
+        host = Optional.ofNullable(System.getenv("HOSTNAME"));
+        port = Optional.ofNullable(System.getenv("PORT"));
+    }
+
+    public static void main( String[] args ) {
+
+        // set application properties
+        appProps.setProperty("server.address", host.orElse("localhost"));
+        appProps.setProperty("server.port", port.orElse("8080"));
+
+        SpringApplication app = new SpringApplication(App.class);
+        app.setDefaultProperties(appProps);
+        app.run(args);
     }
 }
